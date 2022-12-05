ae838bfa43885f92e5e9f2f5ffdaa0bd08ab11e0
Gregory <gregoryhunkeler@gmail.com>
2022-12-05 16:29:31 -0300

    init project

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const cors = require("cors");
+
+const express = require("express");
+// const routes = require("./routes");
+const APP_PORT = process.env.APP_PORT || 3000;
+// const dbInitialSetup = require("./dbInitialSetup");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes(app);
+
+// dbInitialSetup();
+
+app.listen(APP_PORT, () => {
+  console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
+  console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
+});
